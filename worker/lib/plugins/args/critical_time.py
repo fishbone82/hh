5beaee6caf0444ee4d19b43b64c6d02cb5bb5983
Ivan Butorin <i.butorin@corp.mail.ru>
2013-08-09 12:52:43 +0400

    crit/warn time arguments default values changed

--- worker/lib/plugins/args/critical_time.py
+++ worker/lib/plugins/args/critical_time.py
@@ -5,5 +5,5 @@ from base import plugin_arg
 class arg(plugin_arg):
     name = 'critical_time'
     mandatory = 0
-    default_value = 2
+    default_value = 0.5
     force_default = 1
--- worker/lib/plugins/args/warning_time.py
+++ worker/lib/plugins/args/warning_time.py
@@ -5,5 +5,5 @@ from base import plugin_arg
 class arg(plugin_arg):
     name = 'warning_time'
     mandatory = 0
-    default_value = 1
+    default_value = 0.3
     force_default = 1
