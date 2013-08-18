5a1045e18ed2b327af51cc43c22daad4fcdb6246
fishbone <me@fishbone.me>
2013-08-19 02:18:35 +0400

    Mongo connection

--- core/db/connection.py
+++ core/db/connection.py
@@ -1,4 +1,10 @@
+# Mysql section
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 db_engine = create_engine('mysql://hh:hhdbpass@fishbone.me/hh', echo=False)
 Session = sessionmaker(bind=db_engine)
+
+# Mongo section
+from pymongo import Connection
+connection = Connection('fishbone.me', 27017)
+
