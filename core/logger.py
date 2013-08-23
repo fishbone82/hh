16b45bbce0c59e33a6717c497bb0a670bbcef7f4
Ivan Butorin <i.butorin@corp.mail.ru>
2013-08-23 17:15:00 +0400

    logging improvement

--- core/logger.py
+++ core/logger.py
@@ -1,5 +1,6 @@
 from time import strftime
+from os import getpid
 
 
 def log(message='Empty message', originator='UNKNOWN'):
-    print "%s [%s %s] %s" % (strftime('%Y-%m-%d %H:%M:%S'), originator, os.getpid(), message)
+    print "%s [%s %s] %s" % (strftime('%Y-%m-%d %H:%M:%S'), originator, getpid(), message)
