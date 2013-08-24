8d0927228e5615784e57d1f5748dc756cdbd133d
fishbone <me@fishbone.me>
2013-08-25 01:50:25 +0400

    maximum tasks amount set for child

--- core/child.py
+++ core/child.py
@@ -4,6 +4,7 @@ import os
 import requests
 import logger
 WORKER_TIMEOUT = 3
+MAX_TASKS = 1000  # How many tasks can process child in his life
 
 
 def log(message):
@@ -20,7 +21,7 @@ def child_handler(task_queue):
     log("process started")
     signal.signal(signal.SIGTERM, sigterm)
 
-    while True:
+    for i in xrange(MAX_TASKS):
         check = task_queue.get()
         log("got check %s" % check.check_id)
         check_results = []
@@ -44,3 +45,5 @@ def child_handler(task_queue):
                 check_results.append(result)
         log("done check %s: %s" % (check.check_id, check_results))
         check.update_results(check_results)
+    log("I've processed %s tasks. My life is over." % MAX_TASKS)
+    exit(0)
