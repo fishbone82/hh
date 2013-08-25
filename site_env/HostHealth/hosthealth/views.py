53c1baec7667dc29f3faaec279696fc53e5da0d1
fishbone <me@fishbone.me>
2013-08-26 00:58:01 +0400

    login page

--- site_env/HostHealth/hosthealth/views.py
+++ site_env/HostHealth/hosthealth/views.py
@@ -4,3 +4,8 @@ from pyramid.view import view_config
 @view_config(route_name='welcome', renderer='templates/welcome.pt')
 def welcome(request):
     return {'project': 'HostHealth', 'page_title': 'HostHealth::Welcome'}
+
+
+@view_config(route_name='login', renderer='templates/login.pt')
+def login(request):
+    return {'project': 'HostHealth', 'page_title': 'HostHealth::Login'}
