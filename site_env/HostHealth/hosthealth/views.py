e4e61a96f4e1a1ef137928ee8102971583c8859d
Ivan Butorin <i.butorin@corp.mail.ru>
2013-08-27 20:32:34 +0400

    urlparse

--- site_env/HostHealth/hosthealth/views.py
+++ site_env/HostHealth/hosthealth/views.py
@@ -1,5 +1,5 @@
 from pyramid.view import view_config
-
+import urlparse
 
 @view_config(route_name='welcome', renderer='welcome.mako')
 def welcome(request):
@@ -8,5 +8,8 @@ def welcome(request):
 
 @view_config(route_name='login', renderer='login.mako')
 def login(request):
+    query = urlparse.parse_qs(request.query_string)
+    for (k, v) in query.items():
+        query[k] = v[0]
     
     return {'project': 'HostHealth', 'page_title': 'HostHealth::Login'}
