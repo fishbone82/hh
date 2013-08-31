6244f80ae37ea2a635f9efacce705842ed6c7065
fishbone <me@fishbone.me>
2013-08-31 23:58:52 +0400

    thrash

--- www/HostHealth/hosthealth/views/dashboard.py
+++
@@ -1,8 +0,0 @@
-from pyramid.view import view_config
-from base import ViewBase
-
-
-@view_config(route_name='dashboard', renderer='dashboard.jinja2')
-class dashboard(ViewBase):
-    def call(self):
-        return {"ok": 1}
--- www/HostHealth/hosthealth/views/hosts.py
+++
@@ -1,8 +0,0 @@
-from pyramid.view import view_config
-from base import ViewBase
-
-
-@view_config(route_name='hosts', renderer='hosts.jinja2')
-class welcome(ViewBase):
-    def call(self):
-        return {"ok": 1}
